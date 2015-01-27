be826ab758a204d1ef5f872fa2cbe64d995b69ce
snollygolly <snollygolly@gmail.com>
2015-01-26 19:25:08 -0600

    added checks for future shows during watch button clicks #43

--- assets/js/src/show.js
+++ assets/js/src/show.js
@@ -61,7 +61,10 @@ $(document).ready(function() {
     for (var i = 1; i <= season_count; i++) {
       i < 10 ? current_season = ".season-0" + i : current_season = ".season-" + i;
       $(current_season).each(function(index) {
-        episodeArr.push($(this).attr('data-episode'));
+        if ($(this).prop("disabled") != true){
+          //this episode is in the future and shouldn't be watched
+          episodeArr.push($(this).attr('data-episode'));
+        }
       });
     }
 
@@ -76,7 +79,9 @@ $(document).ready(function() {
       }
       if (data.watched === 1) {
         $(current_season).each(function(index) {
-          $(this).prop("checked", true);
+          if ($(this).prop("disabled") != true){
+            $(this).prop("checked", true);
+          }
         });
         while (episodeArr.length > 0) {
           $("#episode-" + episodeArr.pop()).addClass("info");
